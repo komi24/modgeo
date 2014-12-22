d1d97d59923820d6ba363e26d0a274b1b17a9ae7
komi24 <mickael.bolnet@gmail.com>
2014-12-22 10:12:02 +0100

    rm

--- ProgressiveMeshes/acquisition.h
+++
@@ -1,8 +0,0 @@
-
-#include <stdio.h>
-#include <string.h>
-#include "constructeurs.h"  
-#include "listes.h"
-
-int acquisition(char *fichier,float* M);
-// programme qui lit un fichier au format ".NGB" et qui l'analyse afin de transformer le maillage de ce fichier en mailllage memoire utilisable par les fonctions utilisees pour effectuer un traitement "Progressive Mesh". Cette fonction prend en paramatre un flottant M dans lequel elle renverra la plus grande distance entre 2 points du maillage. (ce-ci pourra etre utile comme sentinelle dans certains programmes)
--- ProgressiveMeshes/golf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-make test_pm
-
-test_pm NGB/GOLFCLUB.NGB  res.off $1 tmp   
